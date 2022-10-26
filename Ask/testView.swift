7a69e65e7d360c25f79a05d64f5ebbdfe01e4de3
Mashael Alghunaim <mashael.alghunaim1@gmail.com>
2022-10-26 11:13:09 +0300

    only for test

---
+++ Ask/testView.swift
@@ -0,0 +1,20 @@
+//
+//  testView.swift
+//  Ask
+//
+//  Created by Mashael Alghunaim on 01/04/1444 AH.
+//
+
+import SwiftUI
+
+struct testView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct testView_Previews: PreviewProvider {
+    static var previews: some View {
+        testView()
+    }
+}

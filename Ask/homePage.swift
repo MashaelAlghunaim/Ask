7b9220c47fb80c13b2b43069534521f1a32b5964
Mashael Alghunaim <mashael.alghunaim1@gmail.com>
2022-10-26 11:14:34 +0300

    test only

---
+++ Ask/homePage.swift
@@ -0,0 +1,20 @@
+//
+//  homePage.swift
+//  Ask
+//
+//  Created by Mashael Alghunaim on 01/04/1444 AH.
+//
+
+import SwiftUI
+
+struct homePage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct homePage_Previews: PreviewProvider {
+    static var previews: some View {
+        homePage()
+    }
+}

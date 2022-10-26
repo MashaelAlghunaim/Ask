59cfa66e813ccdc8410055f129710fe4e2090aed
Mashael Alghunaim <mashael.alghunaim1@gmail.com>
2022-10-26 09:56:18 +0300

    Initial Commit

---
+++ Ask/AskApp.swift
@@ -0,0 +1,20 @@
+//
+//  AskApp.swift
+//  Ask
+//
+//  Created by Mashael Alghunaim on 01/04/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct AskApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}

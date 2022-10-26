3dd2dc2c9f0f193b313148b8289e919c8847f343
Mashael Alghunaim <mashael.alghunaim1@gmail.com>
2022-10-26 12:42:54 +0300

    test only

--- SwiftUIView.swift
+++ SwiftUIView.swift
@@ -8,13 +8,37 @@
 import SwiftUI
 
 struct SwiftUIView: View {
-    var body: some View
-    {
-        VStack{
-            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        var body: some View {
+            NavigationView {
+                List {
+                    Section {
+                        Text("About")
+                        Text("Software Update")
+                    }
+                    Section {
+                        Text("AirDrop")
+                        Text("AirPlay & Handoff")
+                        Text("Picture in Picture")
+                        Text("CarPlay")
+                    }
+                    Section {
+                        Text("iPhone Storage")
+                        Text("Background App Refresh")
+                    }
+                    Section {
+                        Text("Date & Time")
+                        Text("Keyboard")
+                        Text("جميع أسئلتي")
+                
+                    }
+                }
+                .navigationTitle("General")
+                .navigationBarTitleDisplayMode(.inline)
+            }
         }
     }
-}
+
+    
 
 struct SwiftUIView_Previews: PreviewProvider {
     static var previews: some View {

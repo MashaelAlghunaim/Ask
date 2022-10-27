2b050f98e9dbb271813defcd595d908028d08c67
AlloTheOne <Allo.busy@gmail.com>
2022-10-27 18:22:04 +0300

    trying something

--- Faves.swift
+++ Faves.swift
@@ -8,17 +8,28 @@
 import SwiftUI
 
 struct Faves: View {
+    @State private var selectedTab = 0
     var body: some View {
         
-        NavigationView {
-            NavigationLink(destination: Text("Second View")){
-                Text("Hello, World!")
-            }
-
-            .navigationTitle("Favorites")
+        ZStack{
+            TabView(selection: $selectedTab, content: {
+                Text("tab content 1").tabItem{ Text("label 1")}.tag(1)
+                Text("tab content 2").tabItem{ Text("label 2")}.tag(2)
+            })
         }
+        
+        
+        
+//        NavigationView {
+//            NavigationLink(destination: Text("Second View")){
+//                Text("Hello, World!")
+//            }
+//            .navigationTitle("Favorites")
+//            .navigationBarTitleDisplayMode(.inline)
+//        }
     }
 }
+
 struct Faves_Previews: PreviewProvider {
     static var previews: some View {
         Faves()
---
+++ Post.swift
@@ -0,0 +1,17 @@
+//
+//  Post.swift
+//  Ask
+//
+//  Created by Alaa Alabdullah on 27/10/2022.
+//
+
+import Foundation
+
+struct  Post : Identifiable {
+    let id = UUID()
+    let authorName: String
+    let timestampText: String
+    let text: String
+    let numberOfLikes: Int
+    
+}
